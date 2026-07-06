c1d2afae84c85e1b716a8e0c253224f1250501c1
Md-Salimm-khan-071 <Salimukhan071@gmail.com>
2026-07-06 19:43:23 +0530

    added some more features to that section like no of tasks for each category , tasks completed out of total tasks

--- tasks_section.js
+++ tasks_section.js
@@ -6,6 +6,31 @@ const taskCategory = document.getElementById("task_category");
 let taskList = document.querySelector(".active_tasks");   // querySelector uses css selectors hence . and # should be used for classes and ids
 let completedTaskList = document.querySelector(".completed_tasks");
 let completedSection = document.querySelector(".completed");
+const completedHeader = document.getElementById("completed_at_header");
+
+
+// for the daily , weekly , milestone toggle
+let selectedCategory = "Daily";  //  .. this says when page starts it should be on daily 
+const dailyButton = document.getElementById("daily_tab");
+const weeklyButton = document.getElementById("weekly_tab");
+const milestoneButton = document.getElementById("milestones_tab");
+
+dailyButton.addEventListener("click", () => {
+    selectedCategory = "Daily";
+    renderTasks();
+});
+
+weeklyButton.addEventListener("click", () => {
+    selectedCategory = "Weekly";
+    renderTasks();
+});
+
+milestoneButton.addEventListener("click", () => {
+    selectedCategory = "Milestones";
+    renderTasks();
+});
+
+// let subtitleTaskCategory = document.getElementById("subtitle"); 
 // console.log(taskList);
 
 function addTask() {
@@ -34,9 +59,15 @@ function addTask() {
 addButton.addEventListener("click", addTask);
 
 function renderTasks(){
+    
     taskList.innerHTML = "";
     completedTaskList.innerHTML = "";
-    for (const task of tasks){
+
+    const filteredTasks = tasks.filter(task => task.category === selectedCategory);
+
+    for (const task of filteredTasks){   
+        relatedCategorySubtitle()
+        lengthOfCategory()
         const taskElement = createTaskCard(task);
         
         const taskLeft = document.createElement("div");   // creates the the container for the left section which has checkbox and task title  
@@ -111,6 +142,8 @@ function renderTasks(){
     else {
         completedSection.style.display = "block";
     }
+
+    updateProgress() // update progress function call 
 }
 
 function createTaskCard(){
@@ -148,6 +181,36 @@ function loadTasks(){
     if (savedTasks) {
         tasks = JSON.parse(savedTasks);
     }
+}
+
+function updateProgress(){
+    const totalTasks = tasks.length;
+    
+    const completedTasks = tasks.filter(task => task.completed).length
+
+    completedHeader.textContent = `${completedTasks}/${totalTasks} Tasks Completed`;  // we already took it D.get.elembyid at th start .
+
+} // call this renderTasks function because all other functions call renderTasks so automatically this function will be called everytime 
+
+function relatedCategorySubtitle(){
+    if (selectedCategory == "Daily"){
+        document.getElementById("subtitle").textContent="𖣠 Small Actions For Today";
+    }
+    else if (selectedCategory == "Weekly"){
+        document.getElementById("subtitle").textContent="𖣠 Steps toward bigger goals this week";
+    }
+    else if (selectedCategory == "Milestones"){
+        document.getElementById("subtitle").textContent="𖣠 Big milestones to work toward";
+    }
+}
+function lengthOfCategory(){
+    const dailyCount = tasks.filter(task => task.category === "Daily").length;
+    const weeklyCount = tasks.filter(task => task.category === "Weekly").length;
+    const milestoneCount = tasks.filter(task => task.category === "Milestones").length;   // reason to add these here and not at the top is : if we add it at top then they will read the empty tasks=[] and never change later showing 0,0,0
+
+    document.getElementById("length_of_daily_task").textContent=`${dailyCount}`
+    document.getElementById("length_of_weekly_task").textContent=`${weeklyCount}`
+    document.getElementById("length_of_milestone_task").textContent=`${milestoneCount}`
 }
 
 loadTasks();
